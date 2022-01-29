346a691c1cd113d4a4415d0f3ae7ce0307e9ed52
Marlyne Hakizimana <47464258+marlhakizi@users.noreply.github.com>
2022-01-28 23:10:42 -0500

    Update testresults.py

--- testresults.py
+++ testresults.py
@@ -1,5 +1,5 @@
 from scraper import  get_channelinfo
 
 def test_results():
-    assert(get_channelinfo("UCM5cUUo5Asa-QgFrBu9L7EQ")=={'xwbD6fL5qC8': {'publishedAt': '2020-11-25T21:59:21Z', 'title': 'CUDA Programming', 'statistics': {'viewCount': '10705', 'likeCount': '288', 'favoriteCount': '0', 'commentCount': '13'}}})
+    assert(get_channelinfo("UCM5cUUo5Asa-QgFrBu9L7EQ")=={'xwbD6fL5qC8': {'publishedAt': '2020-11-25T21:59:21Z', 'title': 'CUDA Programming', 'statistics': {'viewCount': '10717', 'likeCount': '288', 'favoriteCount': '0', 'commentCount': '13'}}})
     
